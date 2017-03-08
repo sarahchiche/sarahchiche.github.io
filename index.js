d893b072a1b96600f59df5f4af634a02a84b3ab7
sarach <sarah@butterfly.ai>
2017-03-08 16:46:22 -0500

    linked the stylesheet and js file to index.html

---
+++ index.js
@@ -0,0 +1,7 @@
+//Declaring a variable named hello and setting to "hello".
+var hello = "hello";
+// Printing to the console the value of hello.
+console.log(hello)
+
+var numbers = 3;
+console.log(numbers)
